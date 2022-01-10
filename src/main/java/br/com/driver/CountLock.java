1fc51e050049b9396abbd81404a5b96bc77995f8
camarguinho <linkcamargo@gmail.com>
2022-01-09 21:41:45 -0300

    Using functional interface Lock

---
+++ src/main/java/br/com/driver/CountLock.java
@@ -0,0 +1,35 @@
+package br.com.driver;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+
+public class CountLock {
+
+    public static int counter;
+    public static Lock lock = new ReentrantLock();
+
+    public static void incrementCounter() {
+        if(lock.tryLock()) {
+            try {
+                int current = counter;
+                System.out.println("Before: " + current + " Current thread: " + Thread.currentThread().getId());
+                counter = current + 1;
+                System.out.println("After: " + counter + " Current thread: " + Thread.currentThread().getId());
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println("You try...");
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread( ()-> incrementCounter());
+            t.start();
+        }
+    }
+}
