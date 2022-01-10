43738caf8f71bf500dcce0b83e2e5c6b8cc66a0e
camarguinho <linkcamargo@gmail.com>
2022-01-10 06:55:50 -0300

    implementing ExecutorService

---
+++ src/main/java/br/com/driver/SingleExecute.java
@@ -0,0 +1,24 @@
+package br.com.driver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleExecute {
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        List<Callable<Integer>> callables = new ArrayList<>();
+        callables.add(() -> 1);
+        callables.add(() -> 2);
+
+        try {
+            Thread.sleep(100);
+            System.out.println(exec.invokeAll(callables));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
