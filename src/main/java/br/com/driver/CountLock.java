5357f79d9068c633f7dc2fb685b85440a737feb1
camarguinho <linkcamargo@gmail.com>
2022-01-09 21:44:32 -0300

    using time for wait

--- src/main/java/br/com/driver/CountLock.java
+++ src/main/java/br/com/driver/CountLock.java
@@ -1,5 +1,6 @@
 package br.com.driver;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.Lock;
 
@@ -9,7 +10,7 @@ public class CountLock {
     public static Lock lock = new ReentrantLock();
 
     public static void incrementCounter() {
-        if(lock.tryLock()) {
+        if(lock.tryLock()){
             try {
                 int current = counter;
                 System.out.println("Before: " + current + " Current thread: " + Thread.currentThread().getId());
@@ -21,10 +22,22 @@ public class CountLock {
         } else {
             System.out.println("You try...");
         }
-
     }
 
-
+    public static void incrementCounterWaiting() throws InterruptedException {
+        if(lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+            try {
+                int current = counter;
+                System.out.println("Before: " + current + " Current thread: " + Thread.currentThread().getId());
+                counter = current + 1;
+                System.out.println("After: " + counter + " Current thread: " + Thread.currentThread().getId());
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println("You try...");
+        }
+    }
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
